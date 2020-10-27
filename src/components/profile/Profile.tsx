8ea118034520f5074a33008b25d50c1c4331fa82
Jérémy Gendre <gendrejeremy@yahoo.fr>
2020-10-27 21:32:44 +0100

    profile section

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -6,20 +6,18 @@ export default function Profile() {
     return (
         <>
             <SectionTitle icon={<FaUserCircle/>} text="PROFIL"/>
-            <div className="flex">
-                <div className="col-sm-8">
-                    <p>Je suis un étudiant en informatique. Je viens de terminer mes 2 ans de DUT Informatique à
-                        l'IUT
-                        d'Annecy-le-vieux et je suis actuellement en licence professionnelle Chargé de Projet
-                        Informatique (CP Info) à Annecy.</p>
-                    <p>Mon parcours m'a permis de découvrir le secteur web qui, depuis, me passionne. Suite à
-                        mes
-                        études, je souhaiterais devenir Développeur Web.</p>
+            <div className="block md:flex mx-8 mt-12 mb-8">
+                <div className="text-2xl my-auto md:w-3/4 xl:mr-20 text-justify">
+                    <p>Je suis un étudiant en informatique. Après avoir obtenu un DUT Informatique à
+                        l'IUT d'Annecy-le-vieux ainsi qu'une licence professionnelle Chargé de Projet Informatique,
+                        je suis actuellement une formation en alternance afin de valider le titre d'Expert en Développement Web au sein de
+                        l'école Ynov à Lyon.</p><br/>
+                    <p>Mon parcours m'a permis de découvrir le secteur web qui, depuis, me passionne. Ainsi, suite à
+                        mes études, je souhaiterais prendre part à un projet dans ce même secteur.</p><br/>
                     En consultant ce site, vous pourrez en apprendre plus sur moi, sur les projets que j'ai pu
-                    réaliser,
-                    etc.
+                    réaliser, et plus encore !
                 </div>
-                <div className="border-4 hover:translate-x-2 hover:-translate-y-2 hover:shadow-profile-hover transform transition duration-300 shadow-profile border-white overflow-hidden rounded w-80">
+                <div className="border-4 mx-auto md:ml-8 mt-8 md:mt-0 lg:w-1/4 lg:h-full lg:rounded h-48 w-48 rounded-full hover:translate-x-2 hover:-translate-y-2 hover:shadow-profile-hover transform transition duration-300 shadow-profile border-white overflow-hidden">
                     <img className="hover:scale-110 transform transition duration-300" src={ process.env.PUBLIC_URL + '/images/perso.jpg' }/>
                 </div>
             </div>
