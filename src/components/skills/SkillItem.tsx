36e9c71247b83aa8749db77372c92a24b01590ad
Jérémy Gendre <gendrejeremy@yahoo.fr>
2020-10-28 22:56:30 +0100

    add all skills item collection

--- src/components/skills/SkillItem.tsx
+++ src/components/skills/SkillItem.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import {SkillItemType} from "../../types/SkillItem";
 
-export default function SkillItem(props: SkillItemType) {
+interface Props extends SkillItemType{
+    fullsize?: boolean
+}
+
+export default function SkillItem(props: Props) {
     return (
-        <div className="flex w-20 h-20 relative transition duration-200 overflow-hidden transform hover:scale-110">
-            <img className="m-auto" alt={"logo-skill-" + props.label} src={props.path}/>
+        <div className="flex mx-2 w-20 h-20 relative transition duration-200 overflow-hidden transform hover:scale-110">
+            <img className={"m-auto " + (props.fullsize ? "w-full h-full" : '')} alt={"logo-skill-" + props.label} src={props.path}/>
             <div className="hidden">
                 <div>
                     {props.label}
--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -6,18 +6,34 @@ import {SkillItemType} from "../../types/SkillItem";
 
 const skillsListProg:Array<SkillItemType> = [
     {label : 'HTML / CSS', path : 'HTML-CSS.png'},
+    {label : 'PHP', path : 'php.png'},
+    {label : 'Javascript', path : 'javascript.png'},
+    {label : 'Typescript', path : 'typescript.png'},
+    {label : 'JSX', path : 'jsx.png'},
+    {label : 'SQL', path : 'sql.webp'},
+    {label : 'Java', path : 'java.svg'},
 ];
 
 const skillsListFrameworks:Array<SkillItemType> = [
     {label : 'Symfony', path : 'symfony.png'},
+    {label : 'React', path : 'react.png'},
+    {label : 'Tailwindcss', path : 'tailwindcss.svg'},
+    {label : 'Axios', path : 'axios.png'},
+    {label : 'Bootstrap', path : 'bootstrap.png'},
 ];
 
 const skillsListSoftwares:Array<SkillItemType> = [
     {label : 'PhpStorm', path : 'Phpstorm.png'},
+    {label : 'Git', path : 'git.png'},
+    {label : 'Postman', path : 'postman.png'},
+    {label : 'Visual studio code', path : 'vscode.png'},
+    {label : 'Office', path : 'office.png'},
 ];
 
 const skillsListLang:Array<SkillItemType> = [
     {label : 'Français', path : 'france.webp'},
+    {label : 'Anglais', path : 'UKflag.webp'},
+    {label : 'Italian', path : 'italy.svg'},
 ];
 
 export default function Skills() {
@@ -25,21 +41,29 @@ export default function Skills() {
         <>
             <SectionTitle icon={<FaTools/>} text="COMPETENCES"/>
             <div className="flex flex-wrap mt-10 text-xl">
-                <div className="w-full lg:w-1/2 my-2">
-                    <div className="mb-4">Langages de programmation</div>
-                    { skillsListProg.map( item => <SkillItem label={item.label} path={process.env.PUBLIC_URL + '/images/skills/' + item.path}/>) }
+                <div className="w-full lg:w-1/2 my-8 px-8">
+                    <div className="mb-4 text-center">Langages de programmation</div>
+                    <div className="flex justify-around">
+                        { skillsListProg.map( item => <SkillItem label={item.label} path={process.env.PUBLIC_URL + '/images/skills/' + item.path}/>) }
+                    </div>
                 </div>
-                <div className="w-full lg:w-1/2 my-2">
-                    <div className="mb-4">Framework & Librairies</div>
-                    { skillsListFrameworks.map( item => <SkillItem label={item.label} path={process.env.PUBLIC_URL + '/images/skills/' + item.path}/>) }
+                <div className="w-full lg:w-1/2 my-8 px-8">
+                    <div className="mb-4 text-center">Frameworks & Librairies</div>
+                    <div className="flex justify-around">
+                        { skillsListFrameworks.map( item => <SkillItem label={item.label} path={process.env.PUBLIC_URL + '/images/skills/' + item.path}/>) }
+                    </div>
                 </div>
-                <div className="w-full lg:w-1/2 my-2">
-                    <div className="mb-4">Logiciels</div>
-                    { skillsListSoftwares.map( item => <SkillItem label={item.label} path={process.env.PUBLIC_URL + '/images/skills/' + item.path}/>) }
+                <div className="w-full lg:w-1/2 my-8 px-8">
+                    <div className="mb-4 text-center">Logiciels</div>
+                    <div className="flex justify-around">
+                        { skillsListSoftwares.map( item => <SkillItem label={item.label} path={process.env.PUBLIC_URL + '/images/skills/' + item.path}/>) }
+                    </div>
                 </div>
-                <div className="w-full lg:w-1/2 my-2">
-                    <div className="mb-4">Langues</div>
-                    { skillsListLang.map( item => <SkillItem label={item.label} path={process.env.PUBLIC_URL + '/images/skills/' + item.path}/>) }
+                <div className="w-full lg:w-1/2 my-8 px-8">
+                    <div className="mb-4 text-center">Langues</div>
+                    <div className="flex justify-around">
+                        { skillsListLang.map( item => <SkillItem label={item.label} path={process.env.PUBLIC_URL + '/images/skills/' + item.path}/>) }
+                    </div>
                 </div>
             </div>
         </>
