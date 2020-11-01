f0d5afbda0661229766b717b111243a24e088b23
Jérémy Gendre <gendrejeremy@yahoo.fr>
2020-11-01 19:43:42 +0100

    add all tags and dispatch them onto each project

--- src/components/projects/ProjectItem.tsx
+++ src/components/projects/ProjectItem.tsx
@@ -11,11 +11,11 @@ export default function ProjectItem(props: ProjectItemType) {
         <div onClick={handleClick} className="project-item flex relative overflow-hidden max-h-2xl max-w-2xl mx-auto my-4 cursor-pointer shadow-project">
             <img alt="project-img" className="transition duration-200" src={props.path}/>
             <div className="project-item-bg absolute top-0 left-0 w-full h-full bg-gray-800 transition duration-200 opacity-40"/>
-            <div onClick={(e) => {e.preventDefault(); e.stopPropagation(); }} className="bottom-0 text-sm left-0 absolute text-white font-bold bg-gray-800 bg-opacity-80 w-full cursor-default">
+            <div onClick={(e) => { e.preventDefault(); e.stopPropagation(); }} className="bottom-0 text-sm left-0 absolute text-white font-bold bg-gray-800 bg-opacity-80 w-full cursor-default">
                 <div className="project-item-text-container relative">
-                    <div className="px-8 py-2 w-full">
+                    <div className="px-8 py-4 w-full">
                         <div className="text-xl">{props.title}</div>
-                        <div className="flex mt-1">
+                        <div className="flex mt-2">
                             {props.tags.map(tag => <ProjectTag color={tag.color} text={tag.text}/>)}
                         </div>
                     </div>
--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -3,46 +3,47 @@ import {FaSuitcase} from "react-icons/all";
 import SectionTitle from "../SectionTitle";
 import ProjectItem from "./ProjectItem";
 import {ProjectItemType} from "../../types/ProjectItemType";
-import {BootstrapTag, JQueryTag, SymfonyTag} from "../../types/Tags";
+import {
+    AxiosTag,
+    BootstrapTag, CsharpTag,
+    JQueryTag,
+    LaravelTag,
+    ReactTag,
+    SymfonyTag,
+    TailwindTag, UnityTag,
+    VuejsTag
+} from "../../types/Tags";
 
 const projectList: Array<ProjectItemType> = [
     {
         path: process.env.PUBLIC_URL + '/images/media-sante.JPG',
         href: 'https://www.media-sante.com/',
         title: 'Media-sante - Appli web interne',
-        tags: [SymfonyTag, BootstrapTag, JQueryTag]
+        tags: [SymfonyTag, JQueryTag, BootstrapTag]
     },
     {
-        path: process.env.PUBLIC_URL + '/images/unity-game.JPG',
+        path: process.env.PUBLIC_URL + '/images/abs_mngt.JPG',
         href: 'https://github.com/JeremyGendre/absence_management_v2',
         title: 'Appli de gestion de congés',
-        tags: [
-            {color: '', text: ''},
-        ]
+        tags: [SymfonyTag, ReactTag, TailwindTag, AxiosTag]
     },
     {
         path: process.env.PUBLIC_URL + '/images/ropeup-api.png',
         href: 'https://www.ropeup.fr/',
         title: 'Rope Up - Appli web',
-        tags: [
-            {color: '', text: ''},
-        ]
+        tags: [LaravelTag, VuejsTag, BootstrapTag]
     },
     {
         path: process.env.PUBLIC_URL + '/images/loudiers-gites.PNG',
         href: 'https://www.gites-de-loudiers.fr/',
         title: 'Gîtes de Loudiers - Site web',
-        tags: [
-            {color: '', text: ''},
-        ]
+        tags: [SymfonyTag, ReactTag, TailwindTag, AxiosTag]
     },
     {
         path: process.env.PUBLIC_URL + '/images/unity-game.JPG',
         href: 'https://unity.com/fr',
         title: 'IUT Annecy - Jeu avec Unity',
-        tags: [
-            {color: '', text: ''},
-        ]
+        tags: [UnityTag, CsharpTag]
     },
 ];
 
