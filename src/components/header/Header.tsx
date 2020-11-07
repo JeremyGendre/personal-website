bce24d8323d701ec23bb119bf653ff07dfa0761b
Jérémy Gendre <gendrejeremy@yahoo.fr>
2020-11-07 23:25:20 +0100

    header submenu fix

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -20,26 +20,28 @@ export default function Header() {
     );
 
     return (
-        <header className="sticky top-0 z-50 flex justify-around bg-gray-800 shadow-header">
-            <div className="flex">
-                <h1 className="my-auto ml-2 text-4xl text-left font-forte text-gray-200">
-                    Jérémy Gendre
-                </h1>
-            </div>
-            <div className="my-auto text-white flex md:hidden">
-                <FiMenu onClick={toggleHeaderCollapse} className="cursor-pointer" size={30}/>
-            </div>
+        <>
+            <header className="sticky top-0 z-50 flex justify-around bg-gray-800 shadow-header">
+                <div className="flex">
+                    <h1 className="my-auto ml-2 text-4xl text-left font-forte text-gray-200">
+                        Jérémy Gendre
+                    </h1>
+                </div>
+                <div className="my-auto text-white flex md:hidden">
+                    <FiMenu onClick={toggleHeaderCollapse} className="cursor-pointer" size={30}/>
+                </div>
+                <div className="hidden md:flex text-white">
+                    { Links }
+                </div>
+            </header>
             { headerCollapsed ? (
-                <div className="flex flex-col md:hidden fixed right-0 bg-gray-800 text-white">
+                <div onClick={() => setHeaderCollapsed(false)} className="flex flex-col md:hidden fixed top-0 right-0 z-50 w-48 right-0 bg-gray-800 text-white">
                     <div className="text-white ml-auto mr-2 mt-2">
                         <CgClose onClick={toggleHeaderCollapse} size={30}/>
                     </div>
                     { Links }
                 </div>
             ) : '' }
-            <div className="hidden md:flex text-white">
-                { Links }
-            </div>
-        </header>
+        </>
     );
 }
